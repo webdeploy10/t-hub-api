984f584a2aa0f5e5d24a905b9e9670d5e03c99c9
Thiago Neves <thiago-antonio.neves@unesp.br>
2020-12-12 18:20:48 -0300

    Minor bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -8,12 +8,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-
-app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 
+
+app.use(cors());
 
 const uri = process.env.ATLAS_URI;
         mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology: true});
