275212abced9f03124519dc122737e7f9dcd8868
Thiago Neves <thiago-antonio.neves@unesp.br>
2020-12-12 17:56:47 -0300

    Final changes (Problem fixed)

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 
 
-app.use(cors());
 const uri = process.env.ATLAS_URI;
         mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology: true});
         const connection = mongoose.connection;
