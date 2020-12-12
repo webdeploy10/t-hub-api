2bbddffd7aa3d948a9c3af504622d19c71946637
Thiago Neves <thiago-antonio.neves@unesp.br>
2020-12-12 17:16:04 -0300

    Adding body-parser

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(express.json());
-
+app.use(bodyParser.json());
 const uri = process.env.ATLAS_URI;
         mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology: true});
         const connection = mongoose.connection;
